ee19b342ba1b1e42a436e77cc17190ee4cc38934
jp <jonhteper@triamseletea.com>
2021-08-27 10:23:41 -0500

    fix: Tablas.java clear unnecessary code

--- Practica7/src/tablas/Tablas.java
+++ Practica7/src/tablas/Tablas.java
@@ -4,8 +4,6 @@ package tablas;
 */
 public class Tablas {
     public static void main(String[] args) {
-        //int max = 10;
-
         for (int i = 1; i <= 10; i++) {
             System.out.printf("\n*** Tabla del %d ***\n", i);
             for (int j = 1; j <= 10; j++) {
