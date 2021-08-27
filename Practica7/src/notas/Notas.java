641174686fe99b5e3b4dabf3107e5798e9dec6f2
jp <jonhteper@triamseletea.com>
2021-08-27 15:42:40 -0500

    feat: New annotations in spanish for Notas.java and Tablas.java

--- Practica7/src/notas/Notas.java
+++ Practica7/src/notas/Notas.java
@@ -14,14 +14,28 @@ package notas;
 * */
 public class Notas {
     public static void main(String[] args) {
+
+        // Ya que las instrucciones piden que los valores sean harcodeados (definidos dentro del código),
+        // definimos las calificaciones...
         double notaExamen = 8,  notaTrabajoGrupo = 9;
-        boolean ratioAsistencia = false;
+
+        // ... y luego definimos si cumple con las asistencias
+        boolean ratioAsistencia = true;
+
+        // Para simplificar el código, podemos guardar en una variable tipo boolean si el alumno cumple con los
+        // porcentajes. Para sacar los porcentajes podemos aplicar una regla de tres:
+        //      para obtener el 60% del examen usamos una regla de tres => `( notaExamen * 60 ) 10`, que simplificado es `(notaExamen * 6)`;
+        //      bajo la misma lógica usamos `(notaTrabajoGrupo * 4)`.
+        // Como obtuvimos el número del porcentaje (p.ej. 60 y 40) y la condición es que el alumno tenga al menos un 5
+        // hacemos la comparación con el 50 %.
         boolean promedio = (notaExamen * 6) + (notaTrabajoGrupo * 4) >= 50;
 
-        if (ratioAsistencia && promedio) {
-            System.out.println("El alumno ha aprobado");
+        if (ratioAsistencia && promedio) { // <-- Comprobamos si el alumno cumple con las dos condiciones
+
+            System.out.println("El alumno ha aprobado \uD83D\uDE03"); // <<-- Pequeña anotación: Java puede imprimir emojis básicos, pero hay que insertarlos de forma hexadecimal
+
         } else {
-            System.out.println("El alumno no ha aprobado");
+            System.out.println("El alumno no ha aprobado \uD83D\uDE1E");
         }
 
 
--- Practica7/src/tablas/Tablas.java
+++ Practica7/src/tablas/Tablas.java
@@ -4,11 +4,19 @@ package tablas;
 */
 public class Tablas {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.printf("\n*** Tabla del %d ***\n", i);
+
+        for (int i = 1; i <= 10; i++) { // <-- Iniciamos un for que recorra los números del 1 al 10
+            System.out.printf("\n*** Tabla del %d ***\n", i); // <-- Anotación estética, no es importante para el resultado
+
+            // Dentro del primer for, creamos otro que también recorra los números del 1 al 10.
+            // Es importante notar que por convención cuando hay varios ciclos for, el primero utiliza la variable 'i',
+            // el segundo la variable 'j', el tercero la variable 'k', etc. Esto no es una ley, pero es considerado una
+            // buena práctica.
             for (int j = 1; j <= 10; j++) {
-                System.out.printf("%d x %d = %d\n", i, j, i*j);
-            }
-        }
-    }
-}
+                System.out.printf("%d x %d = %d\n", i, j, i*j); // <-- En este punto hacemos la multiplicación
+            }// for j
+
+        }// for i
+
+    }// main
+}//class
